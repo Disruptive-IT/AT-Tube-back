511663404b5b865fd9ff16feb23cf1106ed26fa6
Salazar106 <jdps106@hotmail.com>
2024-10-04 09:05:17 -0500

    se genera encarpetado del proyecto

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+import { CorsConfig } from './lib/cors.config.js';
+
+const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(cors(CorsConfig));
+
+export default app;
