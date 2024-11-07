d6f1be02fb223505409a049d1ec6022ae1a81e41
Julian Santiago Millan Rodriguez <126290894+SantiagoMR02@users.noreply.github.com>
2024-11-07 16:31:52 -0500

    Add token googleAuth file

--- src/middlewares/authGoogle.js
+++ src/middlewares/authGoogle.js
@@ -1,5 +1,6 @@
 import passport from 'passport'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import { PrismaClient } from '@prisma/client'
 
@@ -33,8 +34,9 @@ async function (accessToken, refreshToken, profile, cb) {
         }
       })
     }
+    const token = jwt.sign({ id: user.id_users }, process.env.JWT_SECRET, { expiresIn: '1h' })
 
-    return cb(null, user)
+    return cb(null, { user, token })
   } catch (err) {
     console.error('Error in Google OAuth verification:', err)
     return cb(err)
@@ -43,15 +45,17 @@ async function (accessToken, refreshToken, profile, cb) {
 ))
 
 // Serializar el usuario: guarda el `id` del usuario en la sesión
-passport.serializeUser((user, done) => {
-  done(null, user.id_users) // Almacena el `id_users` en la sesión
+passport.serializeUser((data, done) => {
+  // Solo almacena el token en la sesión
+  done(null, data.token)
 })
 
-// Deserializar el usuario: recupera el usuario de la base de datos usando el `id`
-passport.deserializeUser(async (id, done) => {
+passport.deserializeUser(async (token, done) => {
   try {
+    // Verifica y decodifica el token para obtener los datos del usuario
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
     const user = await prisma.users.findUnique({
-      where: { id_users: id }
+      where: { id_users: decoded.id }
     })
     done(null, user)
   } catch (err) {
