9aea2f420a8ce6765a5749ac8d7aaa07a7997a7f
Julian Santiago Millan Rodriguez <126290894+SantiagoMR02@users.noreply.github.com>
2024-12-18 16:01:21 -0500

    Delete image template service

--- src/routes/fileUpload.routes.js
+++ src/routes/fileUpload.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import multerConfig from '../middlewares/multerConfig.js'
-import { uploadAvatarController, createTemplateWithImageController, updateTemplateImageController } from '../controllers/fileUpload.controller.js'
+import { uploadAvatarController, createTemplateWithImageController, updateTemplateImageController, deleteTemplateImageController } from '../controllers/fileUpload.controller.js'
 
 const router = Router()
 
@@ -12,5 +12,8 @@ router.post('/template/design', multerConfig.uploadDesignImages.single('file'), 
 
 // Endpoint para actualizar un template con una imagen, o actualizar la imagen de un template
 router.put('/template/design/update/:template_id', multerConfig.uploadDesignImages.single('file'), updateTemplateImageController)
+
+// Endpoint para eliminar la imagen o diseño del template
+router.delete('/template/delete/image/:template_id', deleteTemplateImageController)
 
 export default router
