8d19796d740b78843f2f7990fd712fc1eb32dec6
Salazar106 <jdps106@hotmail.com>
2024-12-27 10:24:59 -0500

    generate payment  url configuratios

--- src/lib/cors.config.js
+++ src/lib/cors.config.js
@@ -1,4 +1,4 @@
-const allowOrigins = ['https://panelnupak.disruptiveinfotech.com', 'http://localhost:5173', 'https://mercadopago.disruptiveinfotech.com']
+const allowOrigins = ['https://panelnupak.disruptiveinfotech.com', 'http://localhost:5173', 'https://mercadopago.disruptiveinfotech.com', 'https://paypal.disruptiveinfotech.com']
 
 export const CorsConfig = {
   origin: allowOrigins,
