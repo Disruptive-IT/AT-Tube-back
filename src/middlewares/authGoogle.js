98bd1b95a2923dd4ab1df18c9b8b2d3819c57dd1
Julian Santiago Millan Rodriguez <126290894+SantiagoMR02@users.noreply.github.com>
2025-01-03 10:20:03 -0500

    is verified in google auth

--- src/middlewares/authGoogle.js
+++ src/middlewares/authGoogle.js
@@ -31,12 +31,13 @@ async function (accessToken, refreshToken, profile, cb) {
           avatar: profile.photos[0]?.value,
           status: true,
           id_rol: 2,
-          id_contry: 1,
+          id_country: 1,
           str_country: 'COLOMBIA',
           id_city: 330,
           str_city: 'Medellin',
           id_department: 5,
-          str_Department: 'ANTIOQUIA'
+          str_Department: 'ANTIOQUIA',
+          is_verified: true
         }
       })
     }
